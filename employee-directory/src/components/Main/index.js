459e9751d7741e7b87fb6a021445063a2ea7a8a4
Gerson Marroquin <gerson.marroquin@gmail.com>
2020-09-23 20:42:47 -0700

    added tables and able to connect to API to show employee info.

--- employee-directory/src/components/Main/index.js
+++ employee-directory/src/components/Main/index.js
@@ -1,14 +1,38 @@
 import React, { Component } from "react";
 import SearchArea from "../SearchArea";
 import TableArea from "../TableArea";
+import API from "../../utils/API.js";
 import "./style.css";
 
 export default class Main extends Component {
+  state = {
+    employees: [],
+    sortStatus: "D",
+    filteredEmployees: [],
+  };
+  componentDidMount() {
+    API.employeeInfo().then((data) => {
+      console.log("data", data);
+      this.setState({
+        employees: data.data.results,
+        filteredEmployees: data.date.results,
+      });
+    });
+  }
+  employeeSearch = (event) => {
+    // get what they input event.target.value....use filter to filer all employees this.state.employees.filter (keep lower and uppercase in mind)
+  };
+  sortLastName = () => {
+    // add state to say what the current state of function (A and D order)..use if else, sort function pass it a compare function inside the sort function.
+  };
   render() {
     return (
       <>
-        <SearchArea />
-        <TableArea />
+        <SearchArea employeeSearch={this.employeeSearch} />
+        <TableArea
+          employees={this.state.filteredEmployees}
+          sortLastName={this.sortLastName}
+        />
       </>
     );
   }
